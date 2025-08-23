e1abc3913ee7681ea39ce1d4c1b4ba25ac5a7bac
Austin <austinnabilblass@gmail.com>
2025-08-23 00:54:53 -0400

    Mobile Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Home from "./pages/home/home.jsx";
 import Contact from "./pages/contact/Contact.jsx";
 import SignUp from "./pages/signup/SignUp.jsx";
@@ -6,17 +7,54 @@ import Login from "./pages/login/Login.jsx";
 import "./App.css";
 
 function App() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobileView = window.innerWidth <= 768;
+      setIsMobile(isMobileView);
+      if (!isMobileView) {
+        setIsMobileMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    if (isMobile) {
+      setIsMobileMenuOpen(false);
+    }
+  };
+
   return (
     <div>
       <nav className="navbar">
-        <Link to="/" className="logo">
+        <Link to="/" className="logo" onClick={closeMobileMenu}>
           <img src="/icon-large.png" alt="Skill Bytes Logo" />
         </Link>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup" className="signup-link">Get Started</Link></li>
+        
+        <div 
+          className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`} 
+          onClick={toggleMobileMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        
+        <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+          <li><Link to="/" onClick={closeMobileMenu}>Home</Link></li>
+          <li><Link to="/contact" onClick={closeMobileMenu}>Contact</Link></li>
+          <li><Link to="/login" onClick={closeMobileMenu}>Log In</Link></li>
+          <li><Link to="/signup" className="signup-link" onClick={closeMobileMenu}>Get Started</Link></li>
         </ul>
       </nav>
 
